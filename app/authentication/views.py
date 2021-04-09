a872547e81360a037b20607277ad28d5b560cb95
gost3228 <gost1k337@gmail.com>
2021-04-09 13:38:16 +0300

    feat: add yapf formatter

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,8 +1,21 @@
-from rest_framework.viewsets import ViewSet
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
+from .models import User
+from .serializers import UserSerializer
 from .services import compare_verification_code, create_email_verification_code, verify_email
 from .tasks import send_email_confirmation_task
 
 
-class UserViewSet(ViewSet):
-    pass
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    @action(detail=False,
+            methods=['GET'],
+            permission_clasess=[permissions.IsAuthenticated])
+    def current_user(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
