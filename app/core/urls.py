dac8acf8052940b1df44295ade3ed857fe6a2c1c
gost3228 <gost1k337@gmail.com>
2021-04-12 13:28:29 +0300

    feat(core): add drf-yasg (swagger ui)

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny, ),
+)
 
 urlpatterns = [
+    re_path(r'^swagger/$',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
 ]
