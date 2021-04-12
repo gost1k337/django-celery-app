d1a9ec95fcfa94bb843d130627ca6ae5e29b1c38
gost3228 <gost1k337@gmail.com>
2021-04-12 10:14:29 +0300

    feat(auth): add email verification

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -18,6 +18,20 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
 
+    def perform_create(self, serializer):
+        user = serializer.save()
+        verification_code = create_email_verification_code(user.email)
+        send_email_confirmation_task.delay(user.email, verification_code)
+
+    @action(url_path='verify', detail=False, methods=['POST'])
+    def verify(self, request):
+        email = request.user.email
+        code = str(request.data.get('code'))
+        success = compare_verification_code(email=email, code=code)
+        if success:
+            verify_email(email)
+        return Response({"success": success})
+
     def get_permissions(self):
         if self.action == 'create':
             return [permissions.AllowAny()]
